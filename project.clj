12f093121ae87a0940c56a409a5429f9e1e3ab31
Kevin Downey <redchin@gmail.com>
2012-06-14 17:03:32 -0700

    1.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject propS3t "0.0.1"
+(defproject propS3t "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[clojure "1.4.0"]
                  [clj-http "0.4.3"]])
