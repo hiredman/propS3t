dc109c1be767fc9c2a5540c5ab5c1d33f0d6a8fd
Kevin Downey <redchin@gmail.com>
2013-02-21 12:48:58 -0800

    lein 2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject props3t "0.0.3"
   :description "FIXME: write description"
-  :dependencies [[clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.4.3"]])
