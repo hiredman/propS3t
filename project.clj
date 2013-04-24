b940796e48fffd47379aafbb7e1237fa5e6a1385
Kevin Downey <redchin@gmail.com>
2013-04-24 14:56:54 -0700

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject props3t "0.0.4"
+(defproject props3t "0.0.5-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.4.3"]])
