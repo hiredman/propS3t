1bff107d4bf93d8c8a65b5bd0f5b5f6e01d502c8
Kevin Downey <redchin@gmail.com>
2014-03-19 20:08:59 -0700

    lein bikeshed

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject props3t "0.0.5-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-http "0.4.3"]])
+                 [clj-http "0.4.3"]]
+  :plugins [[lein-bikeshed "0.1.2"]])
